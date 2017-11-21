bdd1e4ab384f8e3af0062d2db36e5598e225a7d9
Sergey Kukuev (work) <serg.kukuev@yandex.ru>
2017-11-21 13:12:44 +0300

    Tested async

--- bin/server.js
+++ bin/server.js
@@ -1,5 +1,5 @@
 // Подгрузка модулей
-var http = require('http');
+/*var http = require('http');
 var os = require('os');
 var greeting = require("./my-module/greeting");
 var User = require('./my-module/User');
@@ -11,12 +11,30 @@ var userName = os.userInfo().userName;
 //var port = process.env.PORT || 1337;
 http.createServer(function (request, response) {
     response.writeHead(200, { 'Content-Type': 'text/plain' });
-    response.end(/*sergey.saySalute()/*greeting.getMessage(userName)*/);
+    response.end(/*sergey.saySalute()/*greeting.getMessage(userName)/);
 }).listen(1337, '127.0.0.1', function () {
    //console.log("Date: ${greeting.date}");
     //sergey.displayInfo();
     console.log('${greeting.name}');
     console.log('${greeting2.name}');
     console.log('Server started listening to request on port 1337');
-})
+})*/
 //.listen(port);
+
+function display(data, callback) {
+	// случайное определение ошибки
+	var randInt = Math.random() * (10 - 1) + 1;
+	var err = randInt > 5 ? new Error('Error. randInt > 5.') : null;
+
+	setTimeout(function(){
+		callback(err, data);
+	}, 0);
+}
+
+console.log('Start app');
+display('Data processing...', function(err, data){
+	if (err) throw err;
+	console.log(data);
+});
+
+console.log('Finish app');
