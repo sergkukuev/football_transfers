d8ae6095186411fbd4e4cf126d1c1788a90aed9d
Sergey Kukuev (work) <serg.kukuev@yandex.ru>
2017-11-21 10:56:57 +0300

    Created module 'greeting'

--- RSOI-course/server.js
+++ RSOI-course/server.js
@@ -1,8 +1,15 @@
-'use strict';
+// Подгрузка модулей
 var http = require('http');
-var port = process.env.PORT || 1337;
+var os = require('os');
+var greeting = require("./my-module/greeting");
 
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port);
+var userName = os.userInfo().userName;
+//var port = process.env.PORT || 1337;
+http.createServer(function (request, response) {
+    response.writeHead(200, { 'Content-Type': 'text/plain' });
+    response.end(greeting.getMessage(userName));
+}).listen(1337, '127.0.0.1', function () {
+    console.log("Date: ${greeting.date}");
+    console.log('Server started listening to request on port 1337');
+})
+//.listen(port);
