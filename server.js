917e996b1ee26b16a8df2119beea89e07db21591
Sergey Kukuev (work) <serg.kukuev@yandex.ru>
2017-11-21 14:10:25 +0300

    Tested send html file

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var http = require('http');
+var fs = require('fs');
 
 var port = process.env.PORT || 1337;
 http.createServer(function(request, response){
@@ -6,11 +7,30 @@ http.createServer(function(request, response){
 	console.log('Url: ' + request.url);
 	console.log('Type: ' + request.method);
 	console.log('User-Agent:' + request.headers['user-agent']);
-	console.log(request.headers);*/
+	console.log(request.headers);
 
 	response.setHeader('UserId', 10);
 	response.setHeader('Content-Type', 'text/html');
-	response.write('<h2> My little pony </h2>');
+	response.write('<h2> My little pony </h2>');*/
+
+	console.log('Get Url: ' + request.url);
+	if (request.url.startsWith('/public/')){
+		var fPath = request.url.substr(1);
+		fs.readFile(fPath, function(err, data){
+			if (err) {
+				reponse.statusCode = 404;
+				response.end('Resource not found!');
+			}
+			else {
+				//response.setHeader('Content-Type', 'text/html');
+				response.end(data);
+			}
+			return;
+		})
+	}
+	else {
+		response.end('Missing start page!');
+	}
 
 	response.end();
 }).listen(port);
