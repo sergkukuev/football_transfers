c4effb80506a59d44cf51fcf7d3c9c4ef5779712
Sergey Kukuev (work) <serg.kukuev@yandex.ru>
2017-11-28 13:39:13 +0300

    Installed express module

--- server.js
+++ server.js
@@ -1,14 +1,9 @@
-var http = require('http');
-var fs = require('fs');
-var Calculator = require('./module/arithmetic.js');
+var express = require("express");
+var app = express();
 
-var port = process.env.PORT || 1337;
-http.createServer(function(request, response){
-	fs.readFile('public/index.html', 'utf-8', function(err, data){
-		var calc = new Calculator();
-		data = data.replace('{p1}', 2).replace('{p2}', 4).replace('{pRes}', calc.plus(2, 4));
-		data = data.replace('{m1}', 10).replace('{m2}', 3).replace('{mRes}', calc.minus(10, 3));
-		data = data.replace('{ml1}', 3).replace('{ml2}', 2).replace('{mlRes}', calc.multi(3, 2));
-		response.end(data);
-	})
-}).listen(port);
+// определяем обработчик для маршрута "/"
+app.get("/", function(request, response){
+    response.send("<h2>Привет Express!</h2>");
+});
+
+app.listen(1337);
