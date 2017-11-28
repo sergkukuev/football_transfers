728bfe61bd5daa7529b9d80c0e3ebd62204fafa9
Sergey Kukuev (work) <serg.kukuev@yandex.ru>
2017-11-28 13:48:36 +0300

    Created logs for server

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
-var express = require("express");
+var express         = require('express');
+var path            = require('path'); // модуль для парсинга пути
+var log             = require('./libs/log')(module);
 var app = express();
 
-// определяем обработчик для маршрута "/"
-app.get("/", function(request, response){
-    response.send("<h2>Привет Express!</h2>");
+app.get('/api', function (request, response) {
+    res.send('API is running');
 });
 
-app.listen(1337);
+app.listen(1337, function(){
+    log.info('Express server listening on port 1337');
+});
