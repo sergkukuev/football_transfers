183b705aa5adb591bd16c572212ac5d975197760
Sergey Kukuev (work) <serg.kukuev@yandex.ru>
2017-11-29 10:17:48 +0300

    Created some scheme for db

--- libs/mongoose.js
+++ libs/mongoose.js
@@ -16,35 +16,104 @@ db.once('open', function callback() {
 var Schema = mongoose.Schema;
 
 // Схемы
+// Скауты
 var Scout = new Schema({
-	name: { type: String, required: true },
-	club: { type: String, default: 'NoClub' },
-	age: { type: Number, default: 18 },
+	name: { 
+		type: String,
+		required: true 
+	},
+	club: { 
+		type: String,
+		default: 'NoClub'
+	},
+	age: { 
+		type: Number,
+		default: 18,
+		min: 18,
+		max: 70 
+	},
 	players: [String]
 });
 
+// Игрочишки
 var Player = new Schema({
-	name: { type: String, required: true },
-	club: { type: String, default: 'NoClub' },
-	age: { type: Number, default: 16, min: 16, max: 45 };
-	rank: { type: Number, default: 60, min: 40, max: 100 }, 
-	potential: { type: Number, default: 60, min: 60, max: 100 },
-	cost: { type: Number, default: 0}
+	name: {
+		type: String,
+		required: true
+	},
+	club: { 
+		type: String,
+		default: 'NoClub' 
+	},
+	age: { 
+		type: Number, 
+		default: 16, 
+		min: 16, 
+		max: 45 
+	};
+	rank: { 
+		type: Number, 
+		default: 60, 
+		min: 40, 
+		max: 100 
+	}, 
+	potential: { 
+		type: Number, 
+		default: 60, 
+		min: 60,
+		 max: 100 
+	},
+	cost: { 
+		type: Number,
+		default: 0
+	}
 });
 
+// История трансферов
 var Transfer = new Schema({
-	//players
-	//scouts
-	from: { type: String, default: 'NoClub' },
-	to: { type: String, required: true },
-	spending: { type: Number, default: 0 }
+	player: { 
+		type: String, 
+		required: true 
+	},
+	scout: { 
+		type: String, 
+		defeault: 'NoScout'
+	},
+	from: { 
+		type: String, 
+		default: 'NoClub' 
+	},
+	to: { 
+		type: String, 
+		required: true 
+	},
+	spending: { 
+		type: Number, 
+		default: 0 
+	}
 });
 
-// Валидация 
-/*Article.path('title').validate(function (v) {
-	return v.length > 5 && v.length < 70;
+// Валидация
+Player.path('name').validate(function (v) {
+	return v.length > 1 && v.length < 50
 });
 
-var ArticleModel = mongoose.model('Article', Article);
+Scout.path('name').validate(function (v) {
+	return v.length > 1 && v.length < 50
+});
 
-module.exports.ArticleModel = ArticleModel;*/
+Transfer.path('player').validate(function (v) {
+	return v.length > 1 && v.length < 50
+});
+
+Transfer.path('scout').validate(function (v) {
+	return v.length > 1 && v.length < 50
+});
+
+var ScoutModel = mongoose.model('Scout', Scout);
+var PlayerModel = mongoose.model('Player', Player);
+var TransferModel = mongoose.model('Transfer', Transfer);
+
+module.exports.ScoutModel = ScoutModel;
+module.exports.PlayerModel = PlayerModel;
+module.exports.TransferModel = TransferModel;
