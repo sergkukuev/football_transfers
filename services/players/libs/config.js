7a4e1f492166b5414467258d746797982e14a1d5
Kukuev Sergey <serg.kukuev@yandex.ru>
2017-12-26 13:34:03 +0300

    Added local db

--- services/players/libs/config.js
+++ services/players/libs/config.js
@@ -9,7 +9,8 @@ var config = {
       name: 'playerservice'
     },
     port: process.env.PORT || 3001,
-    db: 'mongodb://admin:admin@rsoicluster-shard-00-00-voyya.mongodb.net:27017,rsoicluster-shard-00-01-voyya.mongodb.net:27017,rsoicluster-shard-00-02-voyya.mongodb.net:27017/test?ssl=true&replicaSet=RsoiCluster-shard-0&authSource=admin'
+    db: 'mongodb://localhost:27017/'
+    //'mongodb://admin:admin@rsoicluster-shard-00-00-voyya.mongodb.net:27017,rsoicluster-shard-00-01-voyya.mongodb.net:27017,rsoicluster-shard-00-02-voyya.mongodb.net:27017/test?ssl=true&replicaSet=RsoiCluster-shard-0&authSource=admin'
   }
 };
 
