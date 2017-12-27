97e5538fa6e13dd5a4be7ca6060acc5d77149acb
Kukuev Sergey (notebook) <serg.kukuev@yandex.ru>
2017-12-27 10:27:27 +0300

    Created bat file for install dependencies

---
+++ services/install_dependencies.bat
@@ -0,0 +1,3 @@
+npm i gulp 
+npm i --save body-parser@1.13.3 chai@3.5.0 chai-http@3.0.0 compression@1.5.2 cookie-parser@1.3.3 express@4.13.3 glob@6.0.4 jade@1.11.0 method-override@2.3.0 mongoose mongoose-rollback morgan@1.6.1 nyc@8.4.0 serve-favicon@2.3.0 winston
+npm i --save-dev jsesc chai@3.5.0 debug@2.2.0 gulp@3.9.0 gulp-nodemon@2.0.2 gulp-livereload@3.8.0 gulp-plumber@1.0.0 mocha@3.0.2 nyc@8.1.0 supertest@2.0.0
