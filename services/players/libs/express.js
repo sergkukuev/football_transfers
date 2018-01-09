51bd90822bcc4312148c6dda9b190a08e367db06
Kukuev Sergey <serg.kukuev@yandex.ru>
2018-01-09 22:46:57 +0300

    Fixed 404 error

--- services/players/libs/express.js
+++ services/players/libs/express.js
@@ -32,7 +32,7 @@ module.exports = function(app, config) {
   });
 
   app.use(function (req, res, next) {
-    var err = new Error('Not Found');
+    var err = new Error('Route not found');
     err.status = 404;
     next(err);
   });
@@ -42,8 +42,7 @@ module.exports = function(app, config) {
       res.status(err.status || 500);
       res.send({
         message: err.message,
-        error: err,
-        title: 'error'
+        error: {},
       });
     });
   }
@@ -53,7 +52,6 @@ module.exports = function(app, config) {
       res.send({
         message: err.message,
         error: {},
-        title: 'error'
       });
   });
 
