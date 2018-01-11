08e98a4e2633e1c8f6ed5fc356871fec45f0220c
Kukuev Sergey <serg.kukuev@yandex.ru>
2018-01-11 23:51:25 +0300

    Updated 500 error

--- services/aggregation/libs/express.js
+++ services/aggregation/libs/express.js
@@ -37,7 +37,7 @@ module.exports = function(app, config) {
   if(app.get('env') === 'development'){
     app.use(function (err, req, res, next) {
       res.status(err.status || 500);
-      res.render({
+      res.render('error', {
       	status : "Error",
         message: err.message
       });
@@ -46,7 +46,7 @@ module.exports = function(app, config) {
 
   app.use(function (err, req, res, next) {
     res.status(err.status || 500);
-    res.render({
+    res.render('error', {
       	status: "Error",
         message: err.message
       });
