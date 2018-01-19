df038b78c12ca710dc40d15263822b7a528c7d60
Sergey Kukuev (work) <serg.kukuev@yandex.ru>
2018-01-19 16:49:07 +0300

    Added error processing

--- application/src/containers/players.vue
+++ application/src/containers/players.vue
@@ -1,10 +1,10 @@
 <template lang="html">
-  <div id="players">
+  <div id="players" v-if="status === 200">
     <div class="notification">
       Number of players per page: &nbsp
-      <input type="text" v-model="count" v-bind:class="count" v-on:change="get_players" style="width:40px"/>
+      <input type="text" v-model="count" v-bind:class="count" v-on:change="update_count" style="width:40px"/>
     </div>
-    <div class="columns">
+    <div class="table">
       <div class="table">
         <table class="table">
           <thead>
@@ -29,10 +29,16 @@
       </div>
     </div>
     <div class="notification">
-      <button v-on:click="prev_page" style="margin-left:40%"> << </button>
-      &nbsp Page {{page + 1}} &nbsp
-      <button v-on:click="next_page"> >> </button>
+    {{data}}
+      <div class="">
+        <button v-on:click="prev_page" style="margin-left:40%"> << </button>
+        &nbsp Page {{page + 1}} &nbsp
+        <button v-on:click="next_page"> >> </button>
+      </div>
     </div>
+  </div>
+  <div v-else class="notification">
+    <label style="margin-left:40%"> {{error.message}} : {{error.response.statusText}} </label>
   </div>
 </template>
 
@@ -46,15 +52,23 @@ export default {
       page: 0,
       count: 10,
       players: [],
-      error: ''
+      status: 200,
+      error: {}
     }
   },
   methods: {
     get_players: function () {
       let path = 'http://127.0.0.1:3000/players?count=' + this.count + '&page=' + this.page
       axios.get(path).then((response) => {
-        this.players = response.data
+        if (response.data.length === 0) {
+          --this.page
+        } else {
+          this.players = response.data
+          this.status = response.status
+        }
       }, (err) => {
+        this.error = err
+        this.status = err.response.status
         console.log(err)
       })
     },
@@ -70,6 +84,10 @@ export default {
       if (this.players.length === 0) {
         this.prev_page()
       }
+    },
+    update_count: function () {
+      this.page = 0
+      this.get_players()
     }
   },
   mounted: function () {
