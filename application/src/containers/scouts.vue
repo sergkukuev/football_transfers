12d9160bb49391cfb8e1d529537df662058a7fd0
Kukuev Sergey <serg.kukuev@yandex.ru>
2018-01-19 19:47:09 +0300

    Updated interface: added error processing

--- application/src/containers/scouts.vue
+++ application/src/containers/scouts.vue
@@ -1,10 +1,10 @@
 <template lang="html">
-  <div id="scouts">
+  <div id="scouts" v-if="status === 200">
     <div class="notification">
       Number of scouts per page: &nbsp
-      <input type="text" v-model="count" v-bind:class="count" v-on:change="get_scouts" style="width:40px"/>
+      <input type="text" v-model="count" v-bind:class="count" v-on:change="update_count" style="width:40px"/>
     </div>
-    <div class="columns">
+    <div class="table">
       <div class="table">
         <table class="table">
           <thead>
@@ -27,17 +27,21 @@
       </div>
     </div>
     <div class="notification">
-      <button v-on:click="prev_page" style="margin-left:40%"> << </button>
-      &nbsp Page {{page + 1}} &nbsp
-      <button v-on:click="next_page"> >> </button>
+    {{data}}
+      <div class="">
+        <button v-on:click="prev_page" style="margin-left:40%"> << </button>
+        &nbsp Page {{page + 1}} &nbsp
+        <button v-on:click="next_page"> >> </button>
+      </div>
     </div>
+  </div>
+  <div v-else class="notification">
+    <label style="margin-left:40%"> {{error.message}} : {{error.response.statusText}} </label>
   </div>
 </template>
 
-        
-
 <script>
-import axios from 'axios'
+import {API} from './api'
 
 export default {
   name: 'scouts',
@@ -46,15 +50,23 @@ export default {
       page: 0,
       count: 10,
       scouts: [],
-      error: ''
+      status: 200,
+      error: {}
     }
   },
   methods: {
     get_scouts: function () {
-      let path = 'http://127.0.0.1:3000/scouts?count=' + this.count + '&page=' + this.page
-      axios.get(path).then((response) => {
-        this.scouts = response.data
+      let path = '/scouts?count=' + this.count + '&page=' + this.page
+      API.get(path).then((response) => {
+        if (response.data.length === 0) {
+          --this.page
+        } else {
+          this.scouts = response.data
+          this.status = response.status
+        }
       }, (err) => {
+        this.error = err
+        this.status = err.response.status
         console.log(err)
       })
     },
@@ -70,6 +82,10 @@ export default {
       if (this.scouts.length === 0) {
         this.prev_page()
       }
+    },
+    update_count: function () {
+      this.page = 0
+      this.get_scouts()
     }
   },
   mounted: function () {
--- application/src/containers/transfers.vue
+++ application/src/containers/transfers.vue
@@ -1,10 +1,10 @@
 <template lang="html">
-  <div id="transfers">
+  <div id="transfers" v-if="status === 200">
     <div class="notification">
       Number of transfers per page: &nbsp
-      <input type="text" v-model="count" v-bind:class="count" v-on:change="get_transfers" style="width:40px"/>
+      <input type="text" v-model="count" v-bind:class="count" v-on:change="update_count" style="width:40px"/>
     </div>
-    <div class="columns">
+    <div class="table">
       <table class="table">
         <thead>
           <tr>
@@ -29,15 +29,21 @@
       </table>
     </div>
     <div class="notification">
-      <button v-on:click="prev_page" style="margin-left:40%"> << </button>
-      &nbsp Page {{page + 1}} &nbsp
-      <button v-on:click="next_page"> >> </button>
+    {{data}}
+      <div class="">
+        <button v-on:click="prev_page" style="margin-left:40%"> << </button>
+        &nbsp Page {{page + 1}} &nbsp
+        <button v-on:click="next_page"> >> </button>
+      </div>
     </div>
+  </div>
+  <div v-else class="notification">
+    <label style="margin-left:40%"> {{error.message}} : {{error.response.statusText}} </label>
   </div>
 </template>
 
 <script>
-import axios from 'axios'
+import {API} from './api'
 
 export default {
   name: 'transfers',
@@ -46,15 +52,23 @@ export default {
       page: 0,
       count: 10,
       transfers: [],
-      error: ''
+      status: 200,
+      error: {}
     }
   },
   methods: {
     get_transfers: function () {
-      let path = 'http://127.0.0.1:3000/transfers?count=' + this.count + '&page=' + this.page
-      axios.get(path).then((response) => {
-        this.transfers = response.data
+      let path = '/transfers?count=' + this.count + '&page=' + this.page
+      API.get(path).then((response) => {
+        if (response.data.length === 0) {
+          --this.page
+        } else {
+          this.transfers = response.data
+          this.status = response.status
+        }
       }, (err) => {
+        this.error = err
+        this.status = err.response.status
         console.log(err)
       })
     },
@@ -70,6 +84,10 @@ export default {
       if (this.transfers.length === 0) {
         this.prev_page()
       }
+    },
+    update_count: function () {
+      this.page = 0
+      this.get_transfers()
     }
   },
   mounted: function () {
