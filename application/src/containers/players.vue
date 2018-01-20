7209dc8254de534448ba7f95984d52122791e336
Kukuev Sergey <serg.kukuev@yandex.ru>
2018-01-20 12:42:27 +0300

    Fixed few errors

--- application/src/containers/players.vue
+++ application/src/containers/players.vue
@@ -1,40 +1,46 @@
 <template lang="html">
-  <div id="players" v-if="status === 200">
-    <input type="hidden" id="selected" value="-" style="width:40px" disabled>
-    <div class="notification">
-      Number of players per page: &nbsp
-      <input type="text" v-model="count" v-bind:class="count" v-on:change="update_count" style="width:40px"/>
+  <div id="players">
+    <input id="selected" type="hidden" disabled>
+    <div id="waiting" class="notification" v-if="status === 0">
+      Loading...
     </div>
-    <table id="play" v-on:click="get_info('play')" class="table">
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Club</th>
-          <th>Rating</th>
-          <th>Contract: Start date</th>
-          <th>Contract: Years</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="item in players">
-          <td>{{item.name}}</td>
-          <td>{{item.club}}</td>
-          <td>{{item.rating}}</td>
-          <td>{{item.contract.date}}</td>
-          <td>{{item.contract.years}}</td>
-        </tr>
-      </tbody>
-    </table>
-    <div class="notification">
-      <div class="">
-        <button v-on:click="prev_page" style="margin-left:40%"> << </button>
-        &nbsp Page {{page + 1}} &nbsp
-        <button v-on:click="next_page"> >> </button>
+    <div id="scout_list" v-else-if="status == 200">
+      <font size="5"> Available players: </font>
+      <div class="notification" >
+        Number of players per page: &nbsp
+        <input type="text" v-model="count" v-bind:class="count" v-on:change="update_count" style="width:40px"/>
+      </div>
+      <table id="play" v-on:click="get_info('play')" class="table">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Club</th>
+            <th>Rating</th>
+            <th>Contract: Start date</th>
+            <th>Contract: Years</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="item in players">
+            <td>{{item.name}}</td>
+            <td>{{item.club}}</td>
+            <td>{{item.rating}}</td>
+            <td>{{item.contract.date}}</td>
+            <td>{{item.contract.years}}</td>
+          </tr>
+        </tbody>
+      </table>
+      <div class="notification">
+        <div class="">
+          <button v-on:click="prev_page" style="margin-left:40%"> << </button>
+          &nbsp Page {{page + 1}} &nbsp
+          <button v-on:click="next_page"> >> </button>
+        </div>
       </div>
     </div>
-  </div>
-  <div v-else class="notification">
-    <label style="margin-left:40%"> {{error.message}} : {{error.response.statusText}} </label>
+    <div v-else class="notification">
+      <font color="red"> Ooooooops. {{error.message}} : {{error.response.statusText}} </font>
+    </div>
   </div>
 </template>
 
@@ -48,7 +54,7 @@ export default {
       page: 0,
       count: 10,
       players: [],
-      status: 200,
+      status: 0,
       error: {}
     }
   },
@@ -59,14 +65,12 @@ export default {
         if (response.data.length === 0) {
           --this.page
         } else {
-          console.log(response.data)
           this.players = response.data
           this.status = response.status
         }
       }, (err) => {
         this.error = err
         this.status = err.response.status
-        console.log(err)
       })
     },
     prev_page: function () {
@@ -91,10 +95,14 @@ export default {
       for (let i = 0; i < table.rows.length; i++) {
         table.rows[i].onclick = function () {
           document.getElementById('selected').value = this.rowIndex
+          console.log('Index read')
         }
       }
       let index = document.getElementById('selected').value
-      window.location = 'http://localhost:8080/players/' + this.players[index - 1].id
+      if (index !== '') {
+        console.log('Index = ' + index)
+        window.location = 'http://localhost:8080/players/' + this.players[index - 1].id
+      }
     }
   },
   mounted: function () {
