39630ad5963a04a166727f89679c521970a08039
Kukuev Sergey <serg.kukuev@yandex.ru>
2018-01-20 17:09:06 +0300

    Fixed some errors

--- application/src/containers/transfer_create.vue
+++ application/src/containers/transfer_create.vue
@@ -1,96 +1,141 @@
   <template lang="html">
   <div id="transfers">
     <p v-if="step !== 3"> Step {{step + 1}} of 3</p>
-    <div class="notification" v-if="step !== 2 && step !== 3">
-        Select one from the table: </br>
-        <input type="hidden" id="selected" value="-" style="width:40px" disabled>
-    </div>
+    <font size="5"> Create transfer: </font>
+    <input type="hidden" id="selected" disabled>
     <div id="step1" v-if="step === 0">
-      Players: </br>
-      <div class="table">
-        <table id="players" class="table" v-on:click="get_row('players')">
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Club</th>
-              <th>Rating</th>
-              <th>Contract: Start date</th>
-              <th>Contract: Years</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="item in items">
-              <td>{{item.name}}</td>
-              <td>{{item.club}}</td>
-              <td>{{item.rating}}</td>
-              <td>{{item.contract.date}}</td>
-              <td>{{item.contract.years}}</td>
-            </tr>
-          </tbody>
-        </table>
+      <font size="3"> Select a player: </font>
+      <div id="waiting" v-if="statusPlayer === 0" class="notification"> 
+        Loading... 
       </div>
-      <div class="notification">
-        <div class="">
+      <div v-else-if="statusPlayer === 200">
+        <div class="notification">
+          Select a player who switches to another club: 
+        </div>
+        <div class="table">
+          <table id="players" class="table" v-on:click="get_row('players')">
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Club</th>
+                <th>Rating</th>
+                <th>Contract: Start date</th>
+                <th>Contract: Years</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="item in items">
+                <td>{{item.name}}</td>
+                <td>{{item.club}}</td>
+                <td>{{item.rating}}</td>
+                <td>{{item.contract.date}}</td>
+                <td>{{item.contract.years}}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <div class="notification">
           <button v-on:click="prev_players" style="margin-left:40%"> << </button>
           &nbsp Page {{page + 1}} &nbsp
           <button v-on:click="next_players"> >> </button>
         </div>
       </div>
+      <div v-else class="notification">
+        <font color="red"> {{error.message}} </font>
+      </div>
     </div>
     <div id="step2" v-else-if="step === 1">
-      Scouts: </br>
-      <div class="table">
-        <table id="scouts" class="table" v-on:click="get_row('scouts')">
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Rank</th>
-              <th>Amount of Deals</th>
-              <th>Amount of Contracts</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="item in items">
-              <td>{{item.name}}</td>
-              <td>{{item.rank}}</td>
-              <td>{{item.amount.deals}}</td>
-              <td>{{item.amount.contracts}}</td>
-            </tr>
-          </tbody>
-        </table>
+      <font size="3"> Select a scout: </font>
+      <div v-if="statusScout === 0" class="notification"> 
+        Loading... 
       </div>
-      <div class="notification">
-        <div class="">
+      <div v-else-if="statusScout === 200">
+        <div class="notification" v-if="step !== 2 && step !== 3">
+          Select a dealer: 
+        </div>
+        <div class="table">
+          <table id="scouts" class="table" v-on:click="get_row('scouts')">
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Rank</th>
+                <th>Amount of Deals</th>
+                <th>Amount of Contracts</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="item in items">
+                <td>{{item.name}}</td>
+                <td>{{item.rank}}</td>
+                <td>{{item.amount.deals}}</td>
+                <td>{{item.amount.contracts}}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <div class="notification">
           <button v-on:click="prev_scouts" style="margin-left:40%"> << </button>
           &nbsp Page {{page + 1}} &nbsp
           <button v-on:click="next_scouts"> >> </button>
-          <button v-on:click="next_step" style="margin-right:10%"> Next step </button>
         </div>
+      </div>
+      <div v-else class="notification">
+        <font color="red"> {{error.message}} </font>
       </div>
     </div>
     <div id="step3" v-else-if="step === 2">
-      <div class="notification">
-        Enter the rest of the information about Transfer:
-        <div>
-          Buying club: </br>
-          <input type="text" id="clubTo" value="NoClub" style="margin-left:5%"/> </br> </br>
-          Transfer cost: </br>
-          <input type="text" id="cost" value="0" style="margin-left:5%"/> </br> </br>
-          Contract years: </br>
-          <select v-model="data.years">
-            <option>1</option>
-            <option>2</option>
-            <option>3</option>
-            <option>4</option>
-            <option>5</option>
-            <option>6</option>
-          </select>
+      <font size="3"> Transfer information: </font>
+      <div class="notification" style="background-color:grey">
+        <div class="notification">
+          <p> Player: </p>
+          <div style="margin-left:40px">
+            <ul style="list-style-type: disc">
+              <li>ID: {{ player.id }}</li>
+              <li>Name: {{ player.name }}</li>
+              <li>Club: {{ player.club }}</li>
+              <li>Age: {{ player.age }}</li>
+              <li>Rating: {{ player.rating }}</li>
+              <li>Contract:</li>
+              <ul>
+                <li>Start date: {{ player.contract.date }}</li>
+                <li>Years: {{ player.contract.years }}</li>
+              </ul>
+            </ul>
+          </div>
+          </br>
+          <p>Scout</p>
+          <div style="margin-left:40px">
+            <ul style="list-style-type: disc">
+              <li>ID: {{scout.id}}</li> 
+              <li>Name: {{ scout.name }}</li>
+              <li>Rank: {{ scout.rank }}</li>
+              <li>Deals: {{ scout.amount.deals }}</li>
+            </ul>
+          </div>
         </div>
+        <div class="notification">
+          Enter the rest of the information about Transfer:
+          <div>
+            Buying club: </br>
+            <input type="text" id="clubTo" value="NoClub" style="margin-left:5%"/> </br> </br>
+            Transfer cost: </br>
+            <input type="text" id="cost" value="0" style="margin-left:5%"/> </br> </br>
+            Contract years: &nbsp
+            <select v-model="data.years">
+              <option>1</option>
+              <option>2</option>
+              <option>3</option>
+              <option>4</option>
+              <option>5</option>
+              <option>6</option>
+            </select>
+          </div>
+        </div>
+        <button v-on:click="next_step" style="margin-right:10%"> Create </button>
       </div>
-      <button v-on:click="next_step" style="margin-right:10%"> Create </button>
     </div>
     <div v-else>
-      <router-link class="" to="/"> Done </router-link>
+      <router-link class="" to="/"> Transfer confirm </router-link>
     </div>
   </div>
 </template>
@@ -103,6 +148,8 @@ export default {
   data: function () {
     return {
       items: [],
+      player: {},
+      scout: {},
       data: {
         playerID: '',
         scoutID: '',
@@ -114,7 +161,9 @@ export default {
       page: 0,
       count: 10,
       step: 0,
-      status: 200,
+      statusPlayer: 0,
+      statusScout: 0,
+      status: 0,
       error: {}
     }
   },
@@ -135,11 +184,11 @@ export default {
           --this.page
         } else {
           this.items = response.data
-          this.status = response
+          this.statusPlayer = response.status
         }
       }, (err) => {
         this.error = err
-        this.status = err.response.status
+        this.statusPlayer = err.response.status
         console.log(err)
       })
     },
@@ -150,11 +199,11 @@ export default {
           --this.page
         } else {
           this.items = response.data
-          this.status = response.status
+          this.statusScout = response.status
         }
       }, (err) => {
         this.error = err
-        this.status = err.response.status
+        this.statusScout = err.response.status
         console.log(err)
       })
     },
@@ -167,7 +216,7 @@ export default {
     next_players: function () {
       ++this.page
       this.get_players()
-      if (this.players.length === 0) {
+      if (this.items.length === 0) {
         this.prev_page()
       }
     },
@@ -180,7 +229,7 @@ export default {
     next_scouts: function () {
       ++this.page
       this.get_scouts()
-      if (this.scouts.length === 0) {
+      if (this.items.length === 0) {
         this.prev_scouts()
       }
     },
@@ -191,28 +240,29 @@ export default {
           document.getElementById('selected').value = this.rowIndex
         }
       }
-      this.next_step()
+      let index = document.getElementById('selected').value
+      if (index !== '') {
+        this.next_step()
+      }
     },
     next_step: function () {
       if (this.step === 0) {
         let sel = document.getElementById('selected').value
-        if (sel !== '-') {
-          this.data.playerID = this.items[sel - 1].id
-          this.data.clubFrom = this.items[sel - 1].club
-          document.getElementById('selected').value = '-'
-          ++this.step
-          this.page = 0
-          this.get_scouts()
-        }
+        this.player = this.items[sel - 1]
+        document.getElementById('selected').value = ''
+        ++this.step
+        this.page = 0
+        this.get_scouts()
       } else if (this.step === 1) {
         let sel = document.getElementById('selected').value
-        if (sel !== '-') {
-          ++this.step
-          this.data.scoutID = this.items[sel - 1].id
-        }
+        this.scout = this.items[sel - 1]
+        ++this.step
       } else if (this.step === 2) {
         let cost = Number(parseInt(document.getElementById('cost').value))
         let club = document.getElementById('clubTo').value
+        this.data.playerID = this.player.id
+        this.data.scoutID = this.scout.id
+        this.data.clubFrom = this.player.club
 
         if (!club || typeof (club) === 'undefined' || club.length === 0 || club.length > 50) {
           console.log('club err')
