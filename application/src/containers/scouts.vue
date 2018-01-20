1808d5b268d76594520fc88f62c720d2df4e2353
Kukuev Sergey <serg.kukuev@yandex.ru>
2018-01-20 12:43:28 +0300

    Updated scout page

--- application/src/containers/scouts.vue
+++ application/src/containers/scouts.vue
@@ -1,41 +1,43 @@
 <template lang="html">
-  <div id="scouts" v-if="status === 200">
-    <div class="notification">
-      Number of scouts per page: &nbsp
-      <input type="text" v-model="count" v-bind:class="count" v-on:change="update_count" style="width:40px"/>
+  <div id="scouts">
+    <div id="waiting" class="notification" v-if="status === 0">
+      Loading...
     </div>
-    <div class="table">
-      <div class="table">
-        <table class="table">
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Rank</th>
-              <th>Amount of Deals</th>
-              <th>Amount of Contracts</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="item in scouts">
-              <td>{{item.name}}</td>
-              <td>{{item.rank}}</td>
-              <td>{{item.amount.deals}}</td>
-              <td>{{item.amount.contracts}}</td>
-            </tr>
-          </tbody>
-        </table>
+    <div id="scout_list" v-else-if="status == 200">
+      <font size="5"> Available scouts: </font>
+      <div class="notification" >
+        Number of scouts per page: &nbsp
+        <input type="text" v-model="count" v-bind:class="count" v-on:change="update_count" style="width:40px"/>
+      </div>
+      <table class="table">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Rank</th>
+            <th>Amount of Deals</th>
+            <th>Amount of Contracts</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="item in scouts">
+            <td>{{item.name}}</td>
+            <td>{{item.rank}}</td>
+            <td>{{item.amount.deals}}</td>
+            <td>{{item.amount.contracts}}</td>
+          </tr>
+        </tbody>
+      </table>
+      <div class="notification">
+        <div class="">
+          <button v-on:click="prev_page" style="margin-left:40%"> << </button>
+          &nbsp Page {{page + 1}} &nbsp
+          <button v-on:click="next_page"> >> </button>
+        </div>
       </div>
     </div>
-    <div class="notification">
-      <div class="">
-        <button v-on:click="prev_page" style="margin-left:40%"> << </button>
-        &nbsp Page {{page + 1}} &nbsp
-        <button v-on:click="next_page"> >> </button>
-      </div>
+    <div v-else class="notification">
+      <font color="red"> Ooooooops. {{error.message}} : {{error.response.statusText}} </font>
     </div>
-  </div>
-  <div v-else class="notification">
-    <label style="margin-left:40%"> {{error.message}} : {{error.response.statusText}} </label>
   </div>
 </template>
 
@@ -49,7 +51,7 @@ export default {
       page: 0,
       count: 10,
       scouts: [],
-      status: 200,
+      status: 0,
       error: {}
     }
   },
@@ -66,7 +68,6 @@ export default {
       }, (err) => {
         this.error = err
         this.status = err.response.status
-        console.log(err)
       })
     },
     prev_page: function () {
