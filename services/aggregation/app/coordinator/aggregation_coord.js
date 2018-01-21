0eed2fbbaadc57f3a116eb93a1760b78e1947307
Kukuev Sergey <serg.kukuev@yandex.ru>
2018-01-21 16:45:24 +0300

    Started working with token in aggr

--- services/aggregation/app/coordinator/aggregation_coord.js
+++ services/aggregation/app/coordinator/aggregation_coord.js
@@ -1,39 +1,170 @@
 const   PlayerHost 	  = 'http://localhost:3001/api',
         ScoutHost	  = 'http://localhost:3002/api',
-        TransferHost  = 'http://localhost:3003/api';
+        TransferHost  = 'http://localhost:3003/api',
+        AuthHost      = 'http://localhost:3005';
+
+const config = require('./../../libs/config'),
+        log  = require('./../../libs/log');
 
 module.exports = {
+    // Auth service
+     getTokenByPassword : function(info, callback){
+        let main_function = function(info, callback){
+            const url = _AuthHost + '/auth/token';
+            const options = createOptions(url, 'POST', AuthToken);
+            const data = {
+                grant_type  : 'password',
+                login       : info.login,
+                password    : info.password
+            };
+            return createAndSendHttpPostRequest(options, data, function(err, status, response){
+                return responseHandlerObject(err, status, response, function(err, status, response){
+                    const repeat = checkServicesInformationFromAuth(status, response, main_function, info, callback);
+                    if (!repeat)
+                        return callback(err, status, response);
+                    return;
+                });
+            });
+        }
+        return main_function(info, callback);
+    },
+    getTokenByToken : function(info, callback){
+        let main_function = function(info, callback){
+            const url = _AuthHost + '/auth/token';
+            const options = createOptions(url, 'POST', AuthToken);
+            const data = {
+                grant_type  : 'refresh_token',
+                refresh_token : info.ref_token
+            };
+            return createAndSendHttpPostRequest(options, data, function(err, status, response){
+                return responseHandlerObject(err, status, response, function(err, status, response){
+                    const repeat = checkServicesInformationFromAuth(status, response, main_function, info, callback);
+                    if (!repeat)
+                        return callback(err, status, response);
+                    return;
+                });                
+            });
+        }
+        return main_function(info, callback);
+    },
+    getUserInfo : function(info, callback){
+        let main_function = function(info, callback){
+            const url = _AuthHost + '/auth/userId';
+            const options = createOptions(url, 'GET', AuthToken, info.token);
+            return createAndSendGetHttpRequest(options, function(err, status, response){
+                return responseHandlerObject(err, status, response, function(err, status, response){
+                    let repeat = checkServicesInformationFromAuth(status, response, main_function, info, callback);
+                    if (!repeat)
+                        callback(err, status, response);
+                    return;
+                });
+            });
+        }
+        return main_function(info, callback);
+    },
     // Players service
-    getPlayers: function (page, count, callback) {
-        const url = PlayerHost + '/players?page=' + page + '&count=' + count;
+    getPlayers: function (data, callback) {
+        const url = PlayerHost + '/players?page=' + data.page + '&count=' + data.count;
         const options = createOptions(url, 'GET');
         createAndSendGetHttpRequest(options, function (err, status, response) {
             return responseHandlerArrayObject(err, status, response, callback);
         });
         return;
     },
-    getPlayer: function (id, callback) {
-        const url = PlayerHost + '/players/' + id;
+    getPlayer: function (data, callback) {
+        const url = PlayerHost + '/players/' + data.id;
         const options = createOptions(url, "GET");
         createAndSendGetHttpRequest(options, function (err, status, response) {
             return responseHandlerObject(err, status, response, callback);
         });
         return;
     },
-    updatePlayerContract: function (id, data, callback) {
-        const url = PlayerHost + '/players/' + id + '/contract/';
+    updatePlayerContract: function (dataContainer, callback) {
+        const url = PlayerHost + '/players/' + dataContainer.id + '/contract/';
         const options = createOptions(url, "PUT");
-        createAndSendHttpPutWithFormRequest(options, data, function (err, status, response) {
+        createAndSendHttpPutWithFormRequest(options, dataContainer.data, function (err, status, response) {
             return responseHandlerObject(err, status, response, callback);
         });
     },
-    updatePlayer: function (id, data, callback) {
-        const url = PlayerHost + '/players/' + id;
+    updatePlayer: function (dataContainer, callback) {
+        const url = PlayerHost + '/players/' + dataContainer.id;
         const options = createOptions(url, "PUT");
-        createAndSendHttpPutWithFormRequest(options, data, function (err, status, response) {
+        createAndSendHttpPutWithFormRequest(options, dataContainer.data, function (err, status, response) {
             return responseHandlerObject(err, status, response, callback);
         });
     },
+    // Scouts service
+    getScouts: function (data, callback) {
+        const url = ScoutHost + '/scouts?page=' + data.page + '&count=' + data.count;
+        const options = createOptions(url, 'GET');
+        createAndSendGetHttpRequest(options, function (err, status, response) {
+            return responseHandlerArrayObject(err, status, response, callback);
+        });
+        return;
+    },
+    getScout: function (data, callback) {
+        const url = ScoutHost + '/scouts/' + data.id;
+        const options = createOptions(url, "GET");
+        createAndSendGetHttpRequest(options, function (err, status, response) {
+            return responseHandlerObject(err, status, response, callback);
+        });
+        return;
+    },
+    incScoutDeals: function (data, callback) {
+        const url = ScoutHost + '/scouts/' + data.id + '/deals/confirm';
+        const options = createOptions(url, "PUT");
+        createAndSendHttpPutWithFormRequest(options, null, function (err, status, response) {
+            return responseHandlerObject(err, status, response, callback);
+        });
+    },
+    decScoutDeals: function (data, callback) {
+        const url = ScoutHost + '/scouts/' + data.id + '/deals/cancel';
+        const options = createOptions(url, "PUT");
+        createAndSendHttpPutWithFormRequest(options, null, function (err, status, response) {
+            return responseHandlerObject(err, status, response, callback);
+        });
+    },
+    incScoutContracts: function (data, callback) {
+        const url = ScoutHost + '/scouts/' + data.id + '/contracts/confirm';
+        const options = createOptions(url, "PUT");
+        createAndSendHttpPutWithFormRequest(options, null, function (err, status, response) {
+            return responseHandlerObject(err, status, response, callback);
+        });
+    },
+    decScoutContracts: function (data, callback) {
+        const url = ScoutHost + '/scouts/' + data.id + '/contracts/cancel';
+        const options = createOptions(url, "PUT");
+        createAndSendHttpPutWithFormRequest(options, null, function (err, status, response) {
+            return responseHandlerObject(err, status, response, callback);
+        });
+    },
+    // Transfers service
+    createTransfer: function (data, callback) {
+        const url = TransferHost + '/transfers/create';
+        const options = createOptions(url, "POST");
+        createAndSendHttpPostRequest(options, data.object, function (err, status, response) {
+            return responseHandlerObject(err, status, response, callback);
+        });
+        return;
+    },
+    getTransfer: function (data, callback) {
+        const url = TransferHost + '/transfers/' + data.id;
+        const options = createOptions(url, "GET");
+        createAndSendGetHttpRequest(options, function (err, status, response) {
+            return responseHandlerObject(err, status, response, callback);
+        });
+        return;
+    },
+    getTransfers: function (data, callback) {
+        const url = TransferHost + '/transfers?page=' + data.page + '&count=' + data.count;
+        const options = createOptions(url, "GET");
+        createAndSendGetHttpRequest(options, function (err, status, response) {
+            return responseHandlerArrayObject(err, status, response, callback);
+        });
+        return;
+    },
+
+    // livecheck services
     livePlayerService: function(callback) {
         const url = PlayerHost + '/players/live';
         const options = createOptions(url, 'HEAD');
@@ -42,97 +173,11 @@ module.exports = {
         });
         return;
     },
-    // Scouts service
-    getScouts: function (page, count, callback) {
-        const url = ScoutHost + '/scouts?page=' + page + '&count=' + count;
-        const options = createOptions(url, 'GET');
-        createAndSendGetHttpRequest(options, function (err, status, response) {
-            return responseHandlerArrayObject(err, status, response, callback);
-        });
-        return;
-    },
-    getScout: function (id, callback) {
-        const url = ScoutHost + '/scouts/' + id;
-        const options = createOptions(url, "GET");
-        createAndSendGetHttpRequest(options, function (err, status, response) {
-            return responseHandlerObject(err, status, response, callback);
-        });
-        return;
-    },
-    incScoutDeals: function (id, callback) {
-        const url = ScoutHost + '/scouts/' + id + '/deals/confirm';
-        const options = createOptions(url, "PUT");
-        createAndSendHttpPutWithFormRequest(options, null, function (err, status, response) {
-            return responseHandlerObject(err, status, response, callback);
-        });
-    },
-    decScoutDeals: function (id, callback) {
-        const url = ScoutHost + '/scouts/' + id + '/deals/cancel';
-        const options = createOptions(url, "PUT");
-        createAndSendHttpPutWithFormRequest(options, null, function (err, status, response) {
-            return responseHandlerObject(err, status, response, callback);
-        });
-    },
-    incScoutContracts: function (id, callback) {
-        const url = ScoutHost + '/scouts/' + id + '/contracts/confirm';
-        const options = createOptions(url, "PUT");
-        createAndSendHttpPutWithFormRequest(options, null, function (err, status, response) {
-            return responseHandlerObject(err, status, response, callback);
-        });
-    },
-    decScoutContracts: function (id, callback) {
-        const url = ScoutHost + '/scouts/' + id + '/contracts/cancel';
-        const options = createOptions(url, "PUT");
-        createAndSendHttpPutWithFormRequest(options, null, function (err, status, response) {
-            return responseHandlerObject(err, status, response, callback);
-        });
-    },
     liveScoutService: function(callback) {
         const url = ScoutHost + '/scouts/live';
         const options = createOptions(url, 'HEAD');
         createAndSendHeadHttpRequest(options, function(err, status) {
             return callback(err, status);
-        });
-        return;
-    },
-    // Transfers service
-    createTransfer: function (object, callback) {
-        const url = TransferHost + '/transfers/create';
-        const options = createOptions(url, "POST");
-        createAndSendHttpPostRequest(options, object, function (err, status, response) {
-            return responseHandlerObject(err, status, response, callback);
-        });
-        return;
-    },
-    updateTransfer: function (id, data, callback) {
-        const url = TransferHost + '/transfers/' + id;
-        const options = createOptions(url, "PUT");
-        createAndSendHttpPutWithFormRequest(options, data, function (err, status, response) {
-            return responseHandlerObject(err, status, response, callback);
-        });
-        return;
-    },
-    getTransfer: function (id, callback) {
-        const url = TransferHost + '/transfers/' + id;
-        const options = createOptions(url, "GET");
-        createAndSendGetHttpRequest(options, function (err, status, response) {
-            return responseHandlerObject(err, status, response, callback);
-        });
-        return;
-    },
-    getTransfers: function (page, count, callback) {
-        const url = TransferHost + '/transfers?page=' + page + '&count=' + count;
-        const options = createOptions(url, "GET");
-        createAndSendGetHttpRequest(options, function (err, status, response) {
-            return responseHandlerArrayObject(err, status, response, callback);
-        });
-        return;
-    },
-    deleteTransfer: function (id, callback) {
-        const url = TransferHost + '/transfers/' + id;
-        const options = createOptions(url, "DELETE");
-        createAndSendDeleteHttpRequest(options, function(err, status, response) {
-            return responseHandlerObject(err, status, response, callback);
         });
         return;
     },
@@ -145,6 +190,11 @@ module.exports = {
         return;
     }
 }
+
+var AuthToken = null,
+    PlayerToken = null,
+    ScoutToken = null, 
+    TransferToken = null;
 
 function createAndSendHttpPutWithFormRequest(options, data, callback) {
     const request = require('request');
@@ -210,8 +260,12 @@ function createOptions(uri, method) {
 }
 
 function responseHandlerObject(err, status, response, callback) {
-    if (err)
-        return callback(err, status, response);
+    if (err) {
+        if (err.code == "ECONNREFUSED")
+            return callback(err, 503, 'Sorry. Service is not available, please try again later');
+        else
+            return callback(err, status, response);
+    }
     else {
         if (response) {
             const object = JSON.parse(response);
@@ -223,8 +277,12 @@ function responseHandlerObject(err, status, response, callback) {
 }
 
 function responseHandlerArrayObject(err, status, response, callback) {
-    if (err)
-        return callback(err, status, response);
+    if (err) {
+        if (err.code == "ECONNREFUSED")
+            return callback(err, 503, 'Sorry. Service is not available, please try again later');
+        else
+            return callback(err, status, response);
+    }
     else {
         if (status == 200) {
             if (response) {
@@ -242,4 +300,65 @@ function responseHandlerArrayObject(err, status, response, callback) {
             }
         }
     }
+}
+
+function checkServicesInformationFromAuth(status, response, method, info, callback){
+    if (status == 401 && response.status == 'Service error'){
+        log.info('Token AuthToken not topical');
+        delete AuthToken;
+        AuthToken = null;
+        method(info, callback);
+        return true;
+    } else if (typeof(response.service) != 'undefined'){
+        log.info('Set new AuthToken Token');
+        AuthToken = response.service;
+        delete response.service;
+    }
+    return false;
+}
+
+function checkServicesInformationFromPlayer(status, response, method, info, callback){
+    if (status == 401 && response.status == 'Service error'){
+        log.info('Token Player not topical');
+        delete PlayerToken;
+        PlayerToken = null;
+        method(info, callback);
+        return true;
+    } else if (typeof(response.service) != 'undefined'){
+        log.info('Set new PlayerToken');
+        PlayerToken = response.service;
+        delete response.service;
+    }
+    return false;
+}
+
+function checkServicesInformationFromScout(status, response, method, info, callback){
+    if (status == 401 && response.status == 'Service error'){
+        log.info('Token Scout not topical');
+        delete ScoutToken;
+        ScoutToken = null;
+        method(info, callback);
+        return true;
+    } else if (typeof(response.service) != 'undefined'){
+        log.info('Set new ScoutToken');
+        ScoutToken = response.service;
+        delete response.service;
+    }
+    return false;
+}
+
+function checkServicesInformationFromTransfer(status, response, method, info, callback){
+    if (status == 401 && response.status == 'Service error'){
+        log.info('Token Trnasfer not topical');
+        delete TransferToken;
+        TransferToken = null;
+        method(info, callback);
+        return true;
+    } 
+    if (response && typeof(response.service) != 'undefined'){
+        log.info('Set new TransferToken');
+        TransferToken = response.service;
+        delete response.service;
+    }
+    return false;
 }
