5d0eb57ad73678619040ada1a27c71ecfd7cdfd6
Kukuev Sergey <serg.kukuev@yandex.ru>
2018-01-21 16:21:45 +0300

    Added id and secret

--- services/aggregation/libs/config.js
+++ services/aggregation/libs/config.js
@@ -6,7 +6,9 @@ var config = {
   development: {
     root: rootPath,
     app: {
-      name: 'aggregationservice'
+      name: 'aggregationservice', 
+      id: 'aggr_id',
+      secret: 'aggr_secret'
     },
     port: process.env.PORT || 3000,
     db: 'mongodb://admin:admin@Cluster0-shard-00-00-voyya.mongodb.net:27017,Cluster0-shard-00-01-voyya.mongodb.net:27017,Cluster0-shard-00-02-voyya.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin'
