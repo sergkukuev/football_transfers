b935c0d4008a58bb35befd56ee42682b8eb5e5f4
Kukuev Sergey <serg.kukuev@yandex.ru>
2018-01-21 16:18:10 +0300

    Revert "Transfered 'libs' folder to 'config'"
    This reverts commit d4b66c2756be4fac6598e7b720fda7e9c1d5a5c0.

--- services/aggregation/libs/config.js
+++ services/aggregation/libs/config.js
@@ -6,9 +6,7 @@ var config = {
   development: {
     root: rootPath,
     app: {
-      name: 'aggregationservice',
-      id: 'aggr_id', 
-      secret: 'aggr_secret'
+      name: 'aggregationservice'
     },
     port: process.env.PORT || 3000,
     db: 'mongodb://admin:admin@Cluster0-shard-00-00-voyya.mongodb.net:27017,Cluster0-shard-00-01-voyya.mongodb.net:27017,Cluster0-shard-00-02-voyya.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin'
