f531fba2ef72df5b08a4455c4fc4ae1a38e9ee1b
Kukuev Sergey <serg.kukuev@yandex.ru>
2018-01-21 15:39:36 +0300

    Added verify authorization

--- services/scouts/app/routes/scouts.js
+++ services/scouts/app/routes/scouts.js
@@ -3,7 +3,8 @@ var express = require('express'),
 	mongoose = require('mongoose'),
 	log = require('./../../config/log')(module),
 	scouts = mongoose.model('Scout'),
-	validator = require('./../validators');
+	validator = require('./../validators'),
+	passport = require('./../passport');
 
 module.exports = function(app) {
 	app.use('/api/scouts', router);
@@ -17,46 +18,58 @@ router.head('/live', function(req, res, next) {
 
 // get all scouts
 router.get('/', function(req, res, next) {
-	let page = validator.checkInt(req.query.page);
-	let count = validator.checkInt(req.query.count);
-	page = validator.checkValue(page) ? page : 0;
-	count = validator.checkValue(count) ? count : 0;
-	scouts.getAll(page, count, function(err, result) {
-		if (err) {
-			log.error('Request \'getAll\': ' + err);
-			res.status(400).send({ status: "Error", message: err});
-		}
-		else {
-			log.info('Request \'getAll\': completed');
-			res.status(200).send(result);
-		}
+	passport.checkServiceAuthorization(req, res, function (scope) {
+		let page = validator.checkInt(req.query.page);
+		let count = validator.checkInt(req.query.count);
+		page = validator.checkValue(page) ? page : 0;
+		count = validator.checkValue(count) ? count : 0;
+		scouts.getAll(page, count, function(err, result) {
+			if (err) {
+				log.error('Request \'getAll\': ' + err);
+				res.status(400).send({ status: "Error", message: err, service: scope});
+			}
+			else {
+				log.info('Request \'getAll\': completed');
+				let temp = {
+					content: result, 
+					service: scope
+				}
+				res.status(200).send(temp);
+			}
+		});
 	});
 });
 
 // get scout by id
 router.get('/:id', function(req, res, next) {
-	const id = req.params.id;
-	scouts.getById(id, function(err, result) {
-		if (err) {
-			if (err.kind == "ObjectId") {
-				log.error('Request \'getById\': Incorrect ID');
-				res.status(400).send({ status: "Error", message: "Incorrect ID"});
-			} 
-			else { 
-				log.error('Request \'getById\': ' + err);
-				res.status(400).send({ status: "Error", message: err});
-			}
-		}
-		else {
-			if (result) {
-				log.info('Request \'getById\': completed');
-				res.status(200).send(result); 
+	passport.checkServiceAuthorization(req, res, function (scope) {
+		const id = req.params.id;
+		scouts.getById(id, function(err, result) {
+			if (err) {
+				if (err.kind == "ObjectId") {
+					log.error('Request \'getById\': Incorrect ID');
+					res.status(400).send({ status: "Error", message: "Incorrect ID", service: scope});
+				} 
+				else { 
+					log.error('Request \'getById\': ' + err);
+					res.status(400).send({ status: "Error", message: err, service: scope});
+				}
 			}
 			else {
-				log.error('Request \'getById\': Scout not found');
-				res.status(404).send({ status: "Error", message: "Scout not found"});	
+				if (result) {
+					log.info('Request \'getById\': completed');
+					let temp = {
+						content: result, 
+						service: scope
+					}
+					res.status(200).send(temp); 
+				}
+				else {
+					log.error('Request \'getById\': Scout not found');
+					res.status(404).send({ status: "Error", message: "Scout not found", service: scope});	
+				}
 			}
-		}
+		});
 	});
 });
 
@@ -80,167 +93,187 @@ router.post('/test_generate', function (req, res, next) {
 		});
 	}
 	log.info('Random scouts was created');
-	res.status(200).send({ status: "Created", message: "Random scouts was created" });
+	res.status(200).send({ status: "Created", message: "Random scouts was created", service: scope});
 });
 
 /////////////////////////////////// PUT REQUEST ///////////////////////////////////
 // increment deals scout by id
 router.put('/:id/deals/confirm', function(req, res, next) {
-	const id = req.params.id;
-	scouts.getById(id, function(err, scout) {
-		if (err) {
-			log.error('Request \'confirmDeals\': ' + err);
-			res.status(400).send({ status: "Error", message: err});
-		}
-		else {
-			let data = {
-				deal: scout.amount.deals + 1,
-				contract: scout.amount.contracts
-			};
-			scouts.updateById(id, data, function(err, result) {
-				if (err) {
-					if (err.kind == "ObjectId") {
-						log.error('Request \'confirmDeals\': Incorrect ID');
-						res.status(400).send({ status: "Error", message: "Incorrect ID"});
-					} 
-					else { 
-						log.error('Request \'confirmDeals\': ' + err);
-						res.status(400).send({ status: "Error", message: err});
-					}
-				}
-				else {
-					if (result) {
-						log.info('Request \'confirmDeals\': completed');
-						let msg = "Deal was confirmed by a " + result.name;
-						res.status(200).send(/*{ status: "Ok", message: msg }*/result);
+	passport.checkServiceAuthorization(req, res, function (scope) {
+		const id = req.params.id;
+		scouts.getById(id, function(err, scout) {
+			if (err) {
+				log.error('Request \'confirmDeals\': ' + err);
+				res.status(400).send({ status: "Error", message: err, service: scope});
+			}
+			else {
+				let data = {
+					deal: scout.amount.deals + 1,
+					contract: scout.amount.contracts
+				};
+				scouts.updateById(id, data, function(err, result) {
+					if (err) {
+						if (err.kind == "ObjectId") {
+							log.error('Request \'confirmDeals\': Incorrect ID');
+							res.status(400).send({ status: "Error", message: "Incorrect ID", service: scope});
+						} 
+						else { 
+							log.error('Request \'confirmDeals\': ' + err);
+							res.status(400).send({ status: "Error", message: err, service: scope});
+						}
 					}
 					else {
-						log.error('Request \'confirmDeals\': Scout not found');
-						res.status(404).send({ status: "Error", message: "Scout not found" });	
-					}
-				} 
-			});
-		}
+						if (result) {
+							log.info('Request \'confirmDeals\': completed');
+							let temp = {
+								content: result, 
+								service: scope
+							}
+							res.status(200).send(temp);
+						}
+						else {
+							log.error('Request \'confirmDeals\': Scout not found');
+							res.status(404).send({ status: "Error", message: "Scout not found", service: scope});	
+						}
+					} 
+				});
+			}
+		});
 	});
 });
 
 // decrement deals scout by id
 router.put('/:id/deals/cancel', function(req, res, next) {
-	const id = req.params.id;
-	scouts.getById(id, function(err, scout) {
-		if (err) {
-			log.error('Request \'cancelDeals\': ' + err);
-			res.status(400).send({ status: "Error", message: err});
-		}
-		else {
-			let data = {
-				deal: scout.amount.deals - 1,
-				contract: scout.amount.contracts
-			};
-			scouts.updateById(id, data, function(err, result) {
-				if (err) {
-					if (err.kind == "ObjectId") {
-						log.error('Request \'cancelDeals\': Incorrect ID');
-						res.status(400).send({ status: "Error", message: "Incorrect ID"});
-					} 
-					else { 
-						log.error('Request \'cancelDeals\': ' + err);
-						res.status(400).send({ status: "Error", message: err});
-					}
-				}
-				else {
-					if (result) {
-						log.info('Request \'cancelDeals\': completed');
-						let msg = "Deal was canceled by a " + result.name;
-						res.status(200).send(/*{ status: "Ok", message: msg });*/result);
+	passport.checkServiceAuthorization(req, res, function (scope) {
+		const id = req.params.id;
+		scouts.getById(id, function(err, scout) {
+			if (err) {
+				log.error('Request \'cancelDeals\': ' + err);
+				res.status(400).send({ status: "Error", message: err, service: scope});
+			}
+			else {
+				let data = {
+					deal: scout.amount.deals - 1,
+					contract: scout.amount.contracts
+				};
+				scouts.updateById(id, data, function(err, result) {
+					if (err) {
+						if (err.kind == "ObjectId") {
+							log.error('Request \'cancelDeals\': Incorrect ID');
+							res.status(400).send({ status: "Error", message: "Incorrect ID", service: scope});
+						} 
+						else { 
+							log.error('Request \'cancelDeals\': ' + err);
+							res.status(400).send({ status: "Error", message: err, service: scope});
+						}
 					}
 					else {
-						log.error('Request \'cancelDeals\': Scout not found');
-						res.status(404).send({ status: "Error", message: "Scout not found" });	
-					}
-				} 
-			});
-		}
+						if (result) {
+							log.info('Request \'cancelDeals\': completed');
+							let temp = {
+								content: result, 
+								service: scope
+							}
+							res.status(200).send(temp);
+						}
+						else {
+							log.error('Request \'cancelDeals\': Scout not found');
+							res.status(404).send({ status: "Error", message: "Scout not found", service: scope});	
+						}
+					} 
+				});
+			}
+		});
 	});
 });
 
 // increment contracts scout by id
 router.put('/:id/contracts/confirm', function(req, res, next) {
-	const id = req.params.id;
-	scouts.getById(id, function(err, scout) {
-		if (err) {
-			log.error('Request \'confirmContract\': ' + err);
-			res.status(400).send({ status: "Error", message: err});
-		}
-		else {
-			let data = {
-				deal: scout.amount.deals,
-				contract: scout.amount.contracts + 1
-			};
-			scouts.updateById(id, data, function(err, result) {
-				if (err) {
-					if (err.kind == "ObjectId") {
-						log.error('Request \'confirmContract\': Incorrect ID');
-						res.status(400).send({ status: "Error", message: "Incorrect ID"});
-					} 
-					else { 
-						log.error('Request \'confirmContract\': ' + err);
-						res.status(400).send({ status: "Error", message: err});
-					}
-				}
-				else {
-					if (result) {
-						log.info('Request \'confirmContract\': completed');
-						let msg = "Contract was confirm by a " + result.name;
-						res.status(200).send(/*{ status: "Ok", message: msg }*/result);
+	passport.checkServiceAuthorization(req, res, function (scope) {
+		const id = req.params.id;
+		scouts.getById(id, function(err, scout) {
+			if (err) {
+				log.error('Request \'confirmContract\': ' + err);
+				res.status(400).send({ status: "Error", message: err, service: scope});
+			}
+			else {
+				let data = {
+					deal: scout.amount.deals,
+					contract: scout.amount.contracts + 1
+				};
+				scouts.updateById(id, data, function(err, result) {
+					if (err) {
+						if (err.kind == "ObjectId") {
+							log.error('Request \'confirmContract\': Incorrect ID');
+							res.status(400).send({ status: "Error", message: "Incorrect ID", service: scope});
+						} 
+						else { 
+							log.error('Request \'confirmContract\': ' + err);
+							res.status(400).send({ status: "Error", message: err, service: scope});
+						}
 					}
 					else {
-						log.error('Request \'confirmContract\': Scout not found');
-						res.status(404).send({ status: "Error", message: "Scout not found" });	
-					}
-				} 
-			});
-		}
+						if (result) {
+							log.info('Request \'confirmContract\': completed');
+							let temp = {
+								content: result, 
+								service: scope
+							}
+							res.status(200).send(temp);
+						}
+						else {
+							log.error('Request \'confirmContract\': Scout not found');
+							res.status(404).send({ status: "Error", message: "Scout not found", service: scope});	
+						}
+					} 
+				});
+			}
+		});
 	});
 });
 
 // increment contracts scout by id
 router.put('/:id/contracts/cancel', function(req, res, next) {
-	const id = req.params.id;
-	scouts.getById(id, function(err, scout) {
-		if (err) {
-			log.error('Request \'cancelContract\': ' + err);
-			res.status(400).send({ status: "Error", message: err});
-		}
-		else {
-			let data = {
-				deal: scout.amount.deals,
-				contract: scout.amount.contracts - 1
-			};
-			scouts.updateById(id, data, function(err, result) {
-				if (err) {
-					if (err.kind == "ObjectId") {
-						log.error('Request \'cancelContract\': Incorrect ID');
-						res.status(400).send({ status: "Error", message: "Incorrect ID"});
-					} 
-					else { 
-						log.error('Request \'cancelContract\': ' + err);
-						res.status(400).send({ status: "Error", message: err});
-					}
-				}
-				else {
-					if (result) {
-						log.info('Request \'cancelContract\': completed');
-						let msg = "Contract was canceled by a " + result.name;
-						res.status(200).send(/*{ status: "Ok", message: msg });*/result);
+	passport.checkServiceAuthorization(req, res, function (scope) {
+		const id = req.params.id;
+		scouts.getById(id, function(err, scout) {
+			if (err) {
+				log.error('Request \'cancelContract\': ' + err);
+				res.status(400).send({ status: "Error", message: err, service: scope});
+			}
+			else {
+				let data = {
+					deal: scout.amount.deals,
+					contract: scout.amount.contracts - 1
+				};
+				scouts.updateById(id, data, function(err, result) {
+					if (err) {
+						if (err.kind == "ObjectId") {
+							log.error('Request \'cancelContract\': Incorrect ID');
+							res.status(400).send({ status: "Error", message: "Incorrect ID", service: scope});
+						} 
+						else { 
+							log.error('Request \'cancelContract\': ' + err);
+							res.status(400).send({ status: "Error", message: err, service: scope});
+						}
 					}
 					else {
-						log.error('Request \'cancelContract\': Scout not found');
-						res.status(404).send({ status: "Error", message: "Scout not found" });	
-					}
-				} 
-			});
-		}
+						if (result) {
+							log.info('Request \'cancelContract\': completed');
+							let temp = {
+								content: result, 
+								service: scope
+							}
+							res.status(200).send(temp);
+						}
+						else {
+							log.error('Request \'cancelContract\': Scout not found');
+							res.status(404).send({ status: "Error", message: "Scout not found", service: scope});	
+						}
+					} 
+				});
+			}
+		});
 	});
 });
 
